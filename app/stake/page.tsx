2807451a4c0434c4750b004eba9b811e4325cf80
CryptoManiacMayn <104672556+CryptoManiacMayn@users.noreply.github.com>
2025-08-17 07:11:42 -0700

    update

--- app/stake/page.tsx
+++ app/stake/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import StakingWidget from "@/components/StakingWidget";
+// change this line:
+import StakingWidget from "../../components/StakingWidget";
 export default function Page() {
   return <StakingWidget />;
 }
