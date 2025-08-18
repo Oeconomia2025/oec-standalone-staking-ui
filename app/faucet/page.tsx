ea34cba218ce085f80544ff9a22db49e71b4e1ee
CryptoManiacMayn <admin@oeconomia.io>
2025-08-18 14:24:04 -0700

    Temp removal

--- app/faucet/page.tsx
+++
@@ -1,9 +0,0 @@
-import FaucetCard from "@/components/FaucetCard";
-
-export default function Page(){
-  return (
-    <div className="main" style={{padding: 20}}>
-      <FaucetCard />
-    </div>
-  );
-}
